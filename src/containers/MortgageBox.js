bdb7b048dc4e5fc01a97483944820ffe7153ed0f
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-14 11:09:02 +0100

    Created container MortgageBox and ensured it can be rendered in App.

---
+++ src/containers/MortgageBox.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class MortgageBox extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I'm the MortgageBox</h1>
+
+      </div>
+    )
+  }
+
+}
+
+export default MortgageBox;
