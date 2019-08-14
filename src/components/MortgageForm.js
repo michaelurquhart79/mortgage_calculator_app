4dfcabf67ae8f523e8a54bb56c2cebf101e0a4ef
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-14 13:12:06 +0100

    Implemented callback function passed as prop from MortgageBox which updates state in MortgageBox when form in Mortgage From is submitted.

--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -9,27 +9,43 @@ class MortgageForm extends Component {
     };
     this.handleUserSalaryChange = this.handleUserSalaryChange.bind(this);
     this.handlePartnerSalaryChange = this.handlePartnerSalaryChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleUserSalaryChange(event) {
-    this.setState({userSalary: parseInt(event.target.value)});
+      this.setState({userSalary: parseInt(event.target.value)});
   }
 
   handlePartnerSalaryChange(event) {
     this.setState({partnerSalary: parseInt(event.target.value)});
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    const userSalary = this.state.userSalary;
+    const partnerSalary = this.state.partnerSalary;
+    if(!userSalary) {
+      return
+    }
+
+    this.props.onFormSubmit(userSalary,partnerSalary);
+
+    this.setState({userSalary: 0, partnerSalary: 0})
+  }
+
   render() {
     return (
       <div className="mortgage-form">
-        <form className="mortgage-form">
+        <form className="mortgage-form" onSubmit={this.handleSubmit}>
           <input
+            min="0"
             type="number"
             placeholder="Your Salary"
             value={this.state.userSalary}
             onChange={this.handleUserSalaryChange}>
           </input>
           <input
+            min="0"
             type="number"
             placeholder="Partners Salary"
             value={this.state.partnerSalary}
--- src/containers/MortgageBox.js
+++ src/containers/MortgageBox.js
@@ -2,11 +2,27 @@ import React, {Component} from 'react';
 import MortgageForm from '../components/MortgageForm';
 
 class MortgageBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      userSalary: 0,
+      partnerSalary: 0,
+      mortgageAwarded: 0
+    }
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit(salary1, salary2){
+    this.setState({userSalary: salary1});
+    this.setState({partnerSalary: salary2});
+  }
+
+
   render() {
     return (
       <div>
         <h1>I'm the MortgageBox</h1>
-        <MortgageForm />
+        <MortgageForm onFormSubmit={this.handleFormSubmit}/>
       </div>
     )
   }
